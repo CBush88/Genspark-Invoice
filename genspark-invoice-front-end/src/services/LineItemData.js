88ecf8795f475b393c8ffc5d16e6d75a39fa2506
CBush88 <christopherbush88@gmail.com>
2022-10-12 11:02:52 -0400

    Fixed URLs in line item service

--- genspark-invoice-front-end/src/services/LineItemData.js
+++ genspark-invoice-front-end/src/services/LineItemData.js
@@ -1,21 +1,21 @@
 import axios from "axios"
 
 export const getLineItems = () => {
-  return axios.get(`http://localhost:8080/lineItems`)
+  return axios.get(`http://localhost:8080/lineitems`)
 }
 
 export const getLineItemById = (id) => {
-    return axios.get(`http://localhost:8080/lineItems/${id}`)
+    return axios.get(`http://localhost:8080/lineitems/${id}`)
 }
 
 export const createLineItem = (lineItem) => {
-    return axios.post(`http://localhost:8080/lineItems`, lineItem)
+    return axios.post(`http://localhost:8080/lineitems`, lineItem)
 }
 
 export const updateLineItem = (lineItem) => {
-    return axios.put(`http://localhost:8080/lineItems`, lineItem)
+    return axios.put(`http://localhost:8080/lineitems`, lineItem)
 }
 
 export const deleteLineItem = (id) => {
-    return axios.delete(`http://localhost:8080/lineItems/${id}`)
+    return axios.delete(`http://localhost:8080/lineitems/${id}`)
 }
